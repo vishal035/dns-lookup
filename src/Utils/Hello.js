1ccc1f96e2482f95b17ae8db827c32e75124082b
vishal035 <kumarvishal880288@gmail.com>
2022-03-29 15:54:56 +0530

    Hello Route is ready

---
+++ src/Utils/Hello.js
@@ -0,0 +1,12 @@
+/*
+ * Hello File
+ *
+ */
+
+// Core Dependencies
+// Main Code
+function hello() {
+  return { Hello: 'World' };
+}
+
+module.exports = hello();
--- src/app.js
+++ src/app.js
@@ -4,11 +4,18 @@
  */
 
 // Core Dependencies
+const hello = require('./Utils/Hello');
+const resData = require('./Dns/DNS');
+
 // 3rd Party Dependencies
 const express = require('express');
 
 // Global Constants
 const app = express();
+
+// Express App Settings
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Global ENV Variables
 const port = 3000;
@@ -16,6 +23,21 @@ const port = 3000;
 // Main Server init Code
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+// DNS Main Lookup Route
+app.get('/dns', (req, res) => {
+  console.log(req.body);
+  const data = resData.lookp(req.body);
+  console.log(data);
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(resData.lookp(req.body));
+});
+
+// Hello Route
+app.get('/hello', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(hello);
 });
 
 // Server Starting
