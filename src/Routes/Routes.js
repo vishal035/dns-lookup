7b03c660cfcc2b5683adf5a96a55875c710f6100
vishal035 <kumarvishal880288@gmail.com>
2022-03-30 18:49:21 +0530

    Routes Are seperated

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -8,4 +8,119 @@
 const express = require('express');
 
 // Global Constants
-var router = express.Router({ caseSensitive: true });
+const router = express.Router({ caseSensitive: true });
+
+// Main Server init Code
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// DNS Main Lookup Route
+/*
+  Postmain Raw Json Input
+  {
+      "domain": "google.com"
+  }
+  */
+
+router.get('/dns', async (req, res) => {
+  // console.log(req.body);
+  // console.log('Data from App.js: ', data);
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.lookUp(req.body));
+});
+
+// Dns Main/Resolve Route
+/*
+  Postmain Raw Json Input
+  {
+      "domain": "dorkyclicks.com",
+      "rrtype": "TXT"
+  }
+      ///////////////////////////////////
+      rrtype can be vary from A, AAAA, CAA, MX,etc
+  */
+router.get('/dns/resolve', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolve(req.body));
+});
+
+// Dns Main/ResolveIP Route
+router.get('/dns/ip', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.reverseIP(req.body));
+});
+
+// Dns Main/ResolveIPV4 Route
+router.get('/dns/ipv4', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveIPV4(req.body));
+});
+
+// Dns Main/ResolveIPV6 Route
+router.get('/dns/ipv6', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveIPV6(req.body));
+});
+
+// Dns Main/ResolveANY Route
+router.get('/dns/any', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveANY(req.body));
+});
+
+// Dns Main/ResolveCNAME Route
+router.get('/dns/cname', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveCNAME(req.body));
+});
+
+// Dns Main/ResolveCAA Route
+router.get('/dns/caa', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveCAA(req.body));
+});
+
+// Dns Main/ResolveMX Route
+router.get('/dns/mx', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveMX(req.body));
+});
+
+// Dns Main/ResolveNAPTR Route
+router.get('/dns/naptr', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveNAPTR(req.body));
+});
+
+// Dns Main/ResolveNS Route
+router.get('/dns/ns', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveNS(req.body));
+});
+
+// Dns Main/ResolvePTR Route
+router.get('/dns/ptr', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolvePTR(req.body));
+});
+
+// Dns Main/ResolveOA Route
+router.get('/dns/soa', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveSOA(req.body));
+});
+
+// Dns Main/ResolveSRV Route
+router.get('/dns/srv', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveSRV(req.body));
+});
+
+// Dns Main/ResolveTXT Route
+router.get('/dns/txt', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send(await Dns.resolveTXT(req.body));
+});
+
+exports.Routes = router;
--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@
 
 // Core Dependencies
 const Hello = require('./Utils/Hello');
-const Dns = require('./Dns/Main');
-
+const Routes = require('./Routes/Routes');
 // 3rd Party Dependencies
 const express = require('express');
 
@@ -16,122 +15,10 @@ const app = express();
 // Express App Settings
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(Routes());
 
 // Global ENV Variables
 const port = 3000;
-
-// Main Server init Code
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// DNS Main Lookup Route
-/*
-Postmain Raw Json Input
-{
-    "domain": "google.com"
-}
-*/
-
-app.get('/dns', async (req, res) => {
-  // console.log(req.body);
-  // console.log('Data from App.js: ', data);
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.lookUp(req.body));
-});
-
-// Dns Main/Resolve Route
-/*
-Postmain Raw Json Input
-{
-    "domain": "dorkyclicks.com",
-    "rrtype": "TXT"
-}
-    ///////////////////////////////////
-    rrtype can be vary from A, AAAA, CAA, MX,etc
-*/
-app.get('/dns/resolve', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolve(req.body));
-});
-
-// Dns Main/ResolveIP Route
-app.get('/dns/ip', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.reverseIP(req.body));
-});
-
-// Dns Main/ResolveIPV4 Route
-app.get('/dns/ipv4', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveIPV4(req.body));
-});
-
-// Dns Main/ResolveIPV6 Route
-app.get('/dns/ipv6', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveIPV6(req.body));
-});
-
-// Dns Main/ResolveANY Route
-app.get('/dns/any', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveANY(req.body));
-});
-
-// Dns Main/ResolveCNAME Route
-app.get('/dns/cname', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveCNAME(req.body));
-});
-
-// Dns Main/ResolveCAA Route
-app.get('/dns/caa', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveCAA(req.body));
-});
-
-// Dns Main/ResolveMX Route
-app.get('/dns/mx', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveMX(req.body));
-});
-
-// Dns Main/ResolveNAPTR Route
-app.get('/dns/naptr', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveNAPTR(req.body));
-});
-
-// Dns Main/ResolveNS Route
-app.get('/dns/ns', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveNS(req.body));
-});
-
-// Dns Main/ResolvePTR Route
-app.get('/dns/ptr', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolvePTR(req.body));
-});
-
-// Dns Main/ResolveOA Route
-app.get('/dns/soa', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveSOA(req.body));
-});
-
-// Dns Main/ResolveSRV Route
-app.get('/dns/srv', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveSRV(req.body));
-});
-
-// Dns Main/ResolveTXT Route
-app.get('/dns/txt', async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(await Dns.resolveTXT(req.body));
-});
 
 // Hello Route
 app.get('/Hello', (req, res) => {
